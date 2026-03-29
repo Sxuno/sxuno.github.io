e05f224dd1f13c150f70819649ae243fc528bd73
Stefan Wienold <stefan.wienold@googlemail.com>
2026-03-29 19:22:31 +0200

    Framework update + added engine eventlistener GPUEnabled + added engine event style class + updated  footer webgpu label + updated script paths + added missing framework scripts + added missing engine scripts + added engine PATH configurations + moved window script to system from user + moved media scripts to system from user

--- system/framework.js
+++ system/framework.js
@@ -1,13 +1,7 @@
-// SDL command layer
-
-// webgpu engine callback
-// if webgpu-engine is supported
-// if webgpu engine is not supported
-// if webgpu engine is aktive
+// TODO:
 
 // layout
 // nav
-
 
 // TODO: 
 	// add cookie loader lang,theme
@@ -22,3 +16,17 @@
 	// ---
 	// console.log(document.styleSheets[1])
 	// document.styleSheets[1].disabled = true
+
+// DOM Loaded
+document.addEventListener('DOMContentLoaded', () => {
+	
+	// engine event listener
+	if (engine) {
+
+		engine.eventdispatcher.addEventListener('GPUEnabled', () => {
+			document.querySelector('.event.GPUEnabled').style.visibility = 'visible'
+		})
+
+	}
+
+})
