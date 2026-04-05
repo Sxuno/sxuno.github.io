89301b178dfdb68d6cf553d2488cf5a55bad4432
Stefan Wienold <stefan.wienold@googlemail.com>
2026-04-05 15:43:59 +0200

    Framewokr update + added view functions + added system functions + removed redundant files

--- system/framework.js
+++ system/framework.js
@@ -1,23 +1,10 @@
-// TODO:
-
-// layout
-// nav
-
 // TODO: 
-	// add cookie loader lang,theme
-	// ---
-	// console.log(document.cookie)
-
-	// add auto language based on system
-	// ---
-	// console.log(`system: ${navigator.language}`)
-
 	// add themeselector
 	// ---
 	// console.log(document.styleSheets[1])
 	// document.styleSheets[1].disabled = true
 
-// DOM Loaded
+// ENGINE hooks
 document.addEventListener('DOMContentLoaded', () => {
 	
 	// engine event listener
@@ -30,3 +17,55 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 })
+// SYSTEM functions
+system = (function () {
+	const session = document.cookie
+	const language = navigator.language
+	const theme = 'default'
+	const notification = null
+	return {session, language, theme}
+})()
+// VIEW functions
+view = (function() {
+	let _cache
+	let _index
+	notification = (function() {
+		const show = () => {}
+		const hide = () => {}
+		return {show, hide}
+	})()
+	container = (function() {
+		let cache = {
+			height : 315*2.5,
+			width : 560*2.5
+		}
+		const show = () => {}
+		const hide = () => {}
+		const page = {
+			next : () => {},
+			prev : () => {}
+		}
+		return {show, hide, page}
+	})()
+	media = (function() {
+		let cache = {
+			image : false,
+			model : false,
+			video : false
+		}
+		const image = () => {}
+		const model = () => {}
+		const video = () => {}
+		return {image, model, video}
+	})()
+	overlay = (function () {
+		let cache = {
+			background : false,
+			func : false
+		}
+		const show = () => {}
+		const hide = () => {}
+		return {show, hide}
+	})()
+	return {container, media, overlay}
+})()
